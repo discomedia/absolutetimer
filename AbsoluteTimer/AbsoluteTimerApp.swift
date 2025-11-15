3d47b7f1cf806405fa3b7987c7b26a1324c236af
Dana Hooshmand <11295476+hooshd@users.noreply.github.com>
2025-11-15 11:29:15 +1100

    Initial Commit

---
+++ AbsoluteTimer/AbsoluteTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsoluteTimerApp.swift
+//  AbsoluteTimer
+//
+//  Created by Dana Hooshmand on 15/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AbsoluteTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
